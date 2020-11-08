dee64aae4b6282da8c0d950a6307ddd2efe2f332
Daniel Cuadra <daniel.cuadra85@gmail.com>
2020-11-07 20:46:35 -0800

    re-order to compare with Ryan N., our code is similar but I can't find the difference

--- models/workout.js
+++ models/workout.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
     date: {
         type: Date,
-        default: () => new Date
+        default: () => new Date()
     },
     exercises: [{
         type: {
@@ -16,24 +16,25 @@ const workoutSchema = new Schema({
         name: {
             type: String,
             trim: true,
-            required: "Please enter the name of the excercise."
+            required: "Please enter the name of the exercise."
+        },
+        duration: {
+            type: Number,
+            required: "Please enter the duration of the exercise."
         },
         weight: {
+            type: Number,
+        },
+        reps: {
             type: Number
         },
         sets: {
             type: Number
         },
-        reps: {
-            type: Number
-        },
         distance: {
             type: Number
         },
-        duration: {
-            type: Number,
-            required: "Please enter the duration of the excercise."
-        },
+
     }]
 
 }, 
@@ -49,5 +50,6 @@ workoutSchema.virtual("totalDuration").get(() => {
     }, 0)
 });
 
-const Exercise = mongoose.model("Exercise", workoutSchema);
-module.exports = Exercise;
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
