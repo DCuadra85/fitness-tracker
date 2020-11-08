a5cda02f74dcf9a814a1aea551f87c0b6c33dfe9
Daniel Cuadra <daniel.cuadra85@gmail.com>
2020-11-07 17:39:21 -0800

    correct errors in server file, more route work needed

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3000;
-
-const User = require("./userModel.js");
+const PORT = process.env.PORT || 8000;
 
 const app = express();
 
@@ -14,6 +12,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+
+app.use(require("./routes/api.js"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
