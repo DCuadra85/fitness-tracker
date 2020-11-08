ff0781148cdce27daff492efd8fb1ae680f823ba
Daniel Cuadra <daniel.cuadra85@gmail.com>
2020-11-07 17:37:37 -0800

    complete routes

--- server.js
+++ server.js
@@ -15,7 +15,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
 app.listen(PORT, () => {
     console.log(`App running on http://localhost:${PORT}`);
